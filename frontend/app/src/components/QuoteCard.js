4312f1f765d1b859aef695550136819b4c1a1258
Dylan Shade <dpshadey22@gmail.com>
2023-04-14 17:29:01 -0400

    @mui/material

--- frontend/app/src/components/QuoteCard.js
+++ frontend/app/src/components/QuoteCard.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import CommentThread from './CommentThread';
+import Button from '@mui/material/Button';
+import styles from './QuoteFeed.module.css';
 
-const QuoteCard = ({ quote }) => {
-    // Sample comment threads data for each quote
-    const commentThreads = [
-        { id: 1, author: 'John', content: 'Great quote!', replies: [] },
-        { id: 2, author: 'Emma', content: 'Thought-provoking.', replies: [] },
-        // More comment threads...
-    ];
-
-    return (
-        <div className="quote-card">
-            <div className="quote">{quote.text}</div>
-            <div className="author">- {quote.author}</div>
-            <CommentThread threads={commentThreads} />
-        </div>
-    );
-};
+function QuoteCard({ text, author }) {
+  return (
+    <div className={styles['quote-card']}>
+      <p className={styles['quote-card__text']}>{text}</p>
+      <p className={styles['quote-card__author']}>{author}</p>
+      <Button variant="contained" color="primary">
+        Read More
+      </Button>
+    </div>
+  );
+}
 
 export default QuoteCard;
